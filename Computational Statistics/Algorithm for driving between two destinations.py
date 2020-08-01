70b1bc2ea8da9ab6078ce91a5ae976595a01ca9b
Alla Topp <59368945+AATopp@users.noreply.github.com>
2020-08-01 18:44:12 -0400

    Create Algorithm for driving between two destinations.py

---
+++ Computational Statistics/Algorithm for driving between two destinations.py
@@ -0,0 +1,10 @@
+# Algorithm for driving between two destinations
+
+print('Head west on Beckenham Blvd for 450 feet')
+print('Turn right onto Beckenham Way and drive for 100 feet')
+print('Turn right to Lynnhaven Pkwy')
+print(' Drive on Lynnhaven Pkwy for 1.5 miles')
+print('Turn right onto S Rosemont Rd')
+print('Drive on S Rosemont Rd for 0.7 miles')
+print('Turn right onto Faculty Blvd')
+print('Drive for 400 feet on Faculty Blvd and your destination will be on the right')
